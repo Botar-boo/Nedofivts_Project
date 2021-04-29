574b4134002b698eff98c26e344abaa6b9f9c299
Sameer Tantry <sameertantry@gmail.com>
2021-04-29 23:48:20 +0300

    tests adding

--- tests/SingletonTest.cpp
+++ tests/SingletonTest.cpp
@@ -3,5 +3,44 @@
 TEST_F(SingletonTestCase, SingletonTest) {
 	Game* game1 = Game::get_instance();
 	Game* game2 = Game::get_instance();
-ASSERT_EQ(game1, game2);
+	ASSERT_EQ(game1, game2);
+}
+
+TEST_F(SingletonTestCase, TypeTest) {
+	Game* game1 = Game::get_instance();
+	Game* game2 = Game::get_instance();
+	ASSERT_EQ(typeid(game1), typeid(game2));
+}
+
+TEST_F(SingletonTestCase, GameOverTest) {
+	Game* game1 = Game::get_instance();
+	game1->startGame(true);
+	ASSERT_EQ(false, game1->get_gameOver());
+}
+
+TEST_F(SingletonTestCase, SwitchGameOverTest) {
+	Game* game1 = Game::get_instance();
+	game1->startGame(true);
+	game1->switch_gameOver();
+	ASSERT_EQ(true, game1->get_gameOver());
+}
+
+TEST_F(SingletonTestCase, Game1SetGameOverTest) {
+	Game* game1 = Game::get_instance();
+	Game* game2 = Game::get_instance();
+	game1->startGame(true);
+	game2->switch_gameOver();
+	ASSERT_EQ(true, game1->get_gameOver());
+}
+
+TEST_F(SingletonTestCase, EazyTest) {
+	Game* game1 = Game::get_instance();
+	game1->startGame(true);
+	ASSERT_EQ(true, game1->get_Hard());
+}
+
+TEST_F(SingletonTestCase, HardTest) {
+	Game* game1 = Game::get_instance();
+	game1->startGame(false);
+	ASSERT_EQ(false, game1->get_Hard());
 }
