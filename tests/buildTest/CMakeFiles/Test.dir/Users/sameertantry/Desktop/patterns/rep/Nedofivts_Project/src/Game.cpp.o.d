438456f7bf6dab8ac3f03a26a36c26eaf4962ce9
Sameer Tantry <sameertantry@gmail.com>
2021-05-08 18:28:35 +0300

    fix test

--- tests/buildTest/CMakeFiles/Test.dir/Users/sameertantry/Desktop/patterns/rep/Nedofivts_Project/src/Game.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Test.dir/Users/sameertantry/Desktop/patterns/rep/Nedofivts_Project/src/Game.cpp.o: \
-  /Users/sameertantry/Desktop/patterns/rep/Nedofivts_Project/src/Game.cpp \
-  /Users/sameertantry/Desktop/patterns/rep/Nedofivts_Project/src/Game.h
